f4fb050b1ed0f3794f7d2606ad2cd7f72f4cca6e
Travis Tidwell <travis@form.io>
2021-11-19 13:26:42 -0600

    Updated code and package.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # async-shell
 A super simple async shell for Node.js
+
+## Installation
+You can install this package using NPM.
+
+```
+npm install async-shell
+```
+
+## Usage
+You can use this library in Node.js within an async/await function.
+
+```
+const shell = require('async-shell');
+
+// Must be within an async function.
+(async () => {
+    const output = await shell('git status');
+    console.log(output);
+})();
+```
---
+++ shell.js
@@ -0,0 +1,27 @@
+const { spawn } = require('child_process');
+const path = require('path');
+const shell = async function (command, directory) {
+  return new Promise((resolve, reject) => {
+    let error = '';
+    let output = '';
+    const options = {};
+    if (directory) {
+        options.cwd = path.join(__dirname, directory);
+    }
+    const _eval = spawn('sh', ['-c', command], options);
+    _eval.stdout.on('data', (data) => {
+      output += data;
+    });
+    _eval.stderr.on('data', (data) => {
+      error += data;
+    });
+    _eval.on('close', (code) => {
+      if (code) {
+        reject(error);
+      } else {
+        resolve(output);
+      }
+    });
+  });
+};
+module.exports = shell;
